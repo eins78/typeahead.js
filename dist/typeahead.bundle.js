ea9671fdee8ca1e4925dd2aef4f3157d440dbbb5
Max F. Albrecht <1@178.is>
2015-06-22 19:24:27 +0200

    v0.11.2-pre.1+forked

--- dist/typeahead.bundle.js
+++ dist/typeahead.bundle.js
@@ -1,5 +1,5 @@
 /*!
- * typeahead.js 0.11.1
+ * typeahead.js 0.11.2-pre.1+forked
  * https://github.com/twitter/typeahead.js
  * Copyright 2013-2015 Twitter, Inc. and other contributors; Licensed MIT
  */
@@ -151,7 +151,7 @@
             noop: function() {}
         };
     }();
-    var VERSION = "0.11.1";
+    var VERSION = "0.11.2-pre.1+forked";
     var tokenizers = function() {
         "use strict";
         return {
